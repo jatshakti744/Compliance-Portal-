95b24d49f4914761b30404f0df594de56fab4194
jatshakti744 <er.shaktijat@gmail.com>
2026-05-28 17:39:34 +0530

    done

--- Frontend/src/App/Layout/AppHeader.tsx
+++ Frontend/src/App/Layout/AppHeader.tsx
@@ -66,17 +66,13 @@ const AppHeader: React.FC = () => {
             {/* Cross Icon */}
           </button>
 
-          <Link to="/" className="lg:hidden">
-            <img
-              className="dark:hidden"
-              src="./images/logo/logo.svg"
-              alt="Logo"
-            />
-            <img
-              className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
-              alt="Logo"
-            />
+          <Link to="/" className="lg:hidden flex items-center gap-2">
+            <div className="flex items-center justify-center w-8 h-8 bg-brand-500 rounded-lg text-white shrink-0">
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+              </svg>
+            </div>
+            <span className="text-xl font-bold text-gray-800 dark:text-white truncate">Compliance Portal</span>
           </Link>
 
           <button
